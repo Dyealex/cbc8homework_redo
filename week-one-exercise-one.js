31942ec70b08b46db16c6c63fd444d5fc14571a8
Dyealex <dyealex@hughes.net>
2020-04-04 05:33:20 -0400

    week-one-exercise-one.html

---
+++ week-one-exercise-one.js
@@ -0,0 +1,29 @@
+// week one homework external javascript file
+
+
+alert ("Hello CBC 8")//to ensure connection to page
+
+// create a variable that holds a String. Add a string to your variable. Log the answer in your console. Load it into the browser and check your answer.
+var a = "One string ";
+var b = "plus another string";
+
+let c = a +''+b;
+// this returns "One string plus another string"
+
+
+// create a variable that holds an integer (is this suppose to be 1 or 2 numbers???)
+var num1 = 8;
+var num2 = 7;
+
+let num3 = 8 * 7;
+console.log(8 * 7)
+
+// the way this is written is sounds like you are suppose to make a variable that stores a string and then a variable that stores a number and multiply the two together. 
+
+var words = "this is the string";
+var int = 7;
+
+let whoKnows = words * int;
+console.log(words * int)
+
+// this returns NaN because I don't think you can multiply a number by a string. Is this the result you were looking for???? This concludes exercise one of week one.
