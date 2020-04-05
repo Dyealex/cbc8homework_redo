2fba2fcb1038cd4c03edebe7e57031f3d8062307
Dyealex <dyealex@hughes.net>
2020-04-05 06:38:35 -0400

    fizzbuzz.html

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+// fizzbuzz.js **week 1 homework**
+
+
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        }  if (i % 3 === 0) {
+            console.log("Fizz")
+        }  if (i % 5 === 0){
+            console.log("Buzz")
+        } if ((i % 3 !=0) && (i % 5 != 0))
+        console.log(i)
+    }
+
+// var test = 10
