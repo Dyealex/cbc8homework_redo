0cea52f2dafb031ee6465f7cf9ace5d5c47795ad
Dyealex <dyealex@hughes.net>
2020-04-05 04:34:20 -0400

    added text

---
+++ Exerciseloop.js
@@ -0,0 +1,10 @@
+// week one homework -part one?- exerciseloop.js
+
+// var exLoop;
+
+for (let exLoop = 1; exLoop < 51; exLoop+=2) {
+    console.log('Exercise loop is working ' + exLoop)
+}
+    console.log('End of loop')
+
+    // This will log every other number 
