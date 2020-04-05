cc11d6d3f4fe7785dcf755c20ffc32f6376b7ebb
Dyealex <dyealex@hughes.net>
2020-04-05 08:57:10 -0400

    added text

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,17 @@
 // fizzbuzz.js **week 1 homework**
 
 
-    for (var i = 1; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz")
-        }  if (i % 3 === 0) {
-            console.log("Fizz")
-        }  if (i % 5 === 0){
-            console.log("Buzz")
-        } if ((i % 3 !=0) && (i % 5 != 0))
-        console.log(i)
-    }
+    for (var i = 1; i <= 100; i++) { // <-- (i <= 100) tells it to run to 100 and stop
 
-// var test = 10
+        if (i % 3 === 0 && i % 5 === 0) { //  <--this checks that it is divisable by 3 && 5
+            console.log("FizzBuzz")
+
+        }  if (i % 3 === 0) { // <--this checks if it is divisable by 3
+            console.log("Fizz")
+
+        }  if (i % 5 === 0){ // <-- this checks if divisable by 5
+            console.log("Buzz")
+
+        } if ((i % 3 !=0) && (i % 5 != 0))  // <-- checks if neither condition is met 
+        console.log(i)
+        }
