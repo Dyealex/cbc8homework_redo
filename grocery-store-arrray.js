17d35cb79ef78624d4d6503ad0c692ddf6614bcf
Dyealex <dyealex@hughes.net>
2020-04-06 04:00:40 -0400

    grocery-store-array.js

---
+++ grocery-store-arrray.js
@@ -0,0 +1,16 @@
+// The Grocery Store Array...
+
+var groceryInventory =[
+    ['Peanut Butter', 3],
+    ['Bananas', 5],
+    ['Yogurt', 6],
+    ['Ground Beef', 8]
+    ['Toilet Paper', 10]
+    ['Paper Towels', 12]
+    ['Special Treats for the Cats', 4]
+    ['Dogfood', 2]
+    ['Zucchini', 7]
+    ['Apple', 10]
+    ['Chicken Tenders', 4]
+];
+console.log(groceryInventory)
