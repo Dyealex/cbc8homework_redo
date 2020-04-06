6a2295cb11e0daec0b2d6c38e4feb213abeefdc5
Dyealex <dyealex@hughes.net>
2020-04-06 02:19:33 -0400

    added text

---
+++ week-one-exercise-function.js
@@ -0,0 +1,36 @@
+//  week-one-exercise-function
+
+
+var numOne = 8;
+var numTwo = 15;
+
+
+// function add () {
+    console.log(numOne + numTwo)
+// }
+
+console.log(numTwo * 5)
+
+
+   for (let numBer = 10; numBer-1 >= 5; numBer--)
+
+    console.log('Does it run: '+ numBer) //I do not understand why this will run as an addition but will not run as a subtraction. wrong symbol, it runs now.
+
+var nextNum = 50
+// if (nextNum + 10 ==60 && nextNum-10 == 40){  //using this line returns 'i hate math'
+
+  if (nextNum + 10 ==60 && nextNum-10 == 45){  //this compares argument 1 (does 50 + 10 = 60 yes to does 50- 10 = 45 no so it returns the else statemnent)
+    console.log('I hate math!')
+    } else {
+    console.log('Math sucks!')
+}
+
+
+
+
+
+
+
+
+
+
